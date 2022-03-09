d33e2082161d691cec7578a5840cc48d66bbab15
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-03-09 22:40:35 +0600

    mailstone 07 practis

---
+++ practis.js
@@ -0,0 +1,73 @@
+// NO 01
+// HOW TO DICLUR LET CONST VARIABLE
+const name = "Minhajul Alam";
+let year = 2020;
+// name = akib;    I WILL NOT CHAING CONST VARIABLW;
+year = 2022; //I WILL CHAING LET VARIAVLE;
+// console.log(name);
+// console.log(year);
+
+// NO 02
+// HOW TO DICLUR A 6WAY CONDITION IN JAVASCRIPT
+const phonePrice = 5000;
+
+// FIRS > CONDITION
+if (phonePrice > 4000) {
+  //   console.log("i will bye a phone");
+} else {
+  //   console.log("oh shit i will try a next time");
+}
+// SECOND < CONDITION
+if (phonePrice < 6000) {
+  //   console.log("yeh i will bye a new mibile");
+} else {
+  //   console.log("i will try next time");
+}
+// THIRD  === CONDITION
+if (phonePrice === 5000) {
+  //   console.log("yea i have a new phone");
+} else {
+  //   console.log("shit this is bed time");
+}
+
+// FORTH !== CONDITION
+if (phonePrice !== 2000) {
+  //   console.log("yes");
+} else {
+  //   console.log("no");
+}
+// FIVE >= CONDITION
+if (phonePrice >= 4000) {
+  //   console.log("yeah this is valid");
+} else {
+  //   console.log("this is wrong");
+}
+// six <= CONDITION
+if (phonePrice <= 4000) {
+  //   console.log("you win ");
+} else {
+  //   console.log("you loss");
+}
+
+// NOM 03
+// HOW TI DICLURE AN ATRREY
+const febNum = [20, 54, 62, 65, 65, 20];
+// CHECK ARREY LENGTH
+// console.log(febNum.length);
+// CHECK ARREY INDEX
+// console.log(febNum[3]);
+// CHECK ARREY INDEXOf
+// console.log(febNum.indexOf(65));
+// CHECK ARREY PUSH
+// console.log(febNum.push(10));
+// CHECK ARREY pop
+// console.log(febNum.pop());
+// CHECK ARREY INCLUDES
+// console.log(febNum.includes(65));
+// console.log(febNum);
+
+// NO 04
+// HOW TO WORK FOR LOOP IN ARREY
+for (let i = 0; i < febNum[i]; i++) {
+  console.log(i);
+}
